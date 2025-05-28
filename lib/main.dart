b76e3a95a6c77254f28e89cc37b528c3bf2b532b
Bruno Melo <brunomelohwm@gmail.com>
2025-05-28 16:02:14 -0300

    chore: add dotenv load in main

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter/widgets.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:noct_cinedex/root_app.dart';
 import 'package:noct_cinedex/injection_container.dart' as di;
 
-void main() async {
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load(fileName: "assets/config/.env");
 
   await SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
 

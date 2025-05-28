6fdc46be33182f0e5a6bf35726d578985afce278
Bruno Melo <brunomelohwm@gmail.com>
2025-05-28 16:03:27 -0300

    chore: changed reference for api key based in dot env

--- lib/core/network/dio_client.dart
+++ lib/core/network/dio_client.dart
@@ -1,5 +1,5 @@
 import 'package:dio/dio.dart';
-import 'package:noct_cinedex/core/config/api_keys.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:noct_cinedex/core/network/api_key_interceptor.dart';
 import 'package:noct_cinedex/core/network/api_urls.dart';
 
@@ -11,7 +11,7 @@ class DioClient {
         connectTimeout: const Duration(seconds: 10),
         receiveTimeout: const Duration(seconds: 10),
       ),
-    )..interceptors.add(ApiKeyInterceptor(tmdbApiKey));
+    )..interceptors.add(ApiKeyInterceptor(dotenv.env['TMDB_API_KEY'] ?? ''));
 
     return dio;
   }

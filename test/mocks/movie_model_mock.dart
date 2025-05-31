121977e175f6493934dfafab03e1c4b82d27eed1
Bruno Melo <brunomelohwm@gmail.com>
2025-05-31 20:14:29 -0300

    feat(test): add mock movie model data for movies

---
+++ test/mocks/movie_model_mock.dart
@@ -0,0 +1,13 @@
+import 'package:noct_cinedex/data/models/movies/movie_model.dart';
+
+final tMovieModel = MovieModel(
+  idModel: 1,
+  genresIdModel: [2, 3, 4],
+  overviewModel: 'sinopse do filme',
+  posterPathModel: 'poster/path',
+  releaseDateModel: '1977',
+  titleModel: 'Xenogears the Movie',
+  voteAverageModel: 95,
+);
+
+final tMovieList = [tMovieModel];

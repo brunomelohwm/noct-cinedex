0c401a201f667ae4dc4c09290c21e83084d3ba80
Bruno Melo <brunomelohwm@gmail.com>
2025-05-31 20:12:13 -0300

    feat(test): add mock Json data for movies

---
+++ test/mocks/movie_json_string_mock.dart
@@ -0,0 +1,11 @@
+final tJsonString = '''
+  {
+    "id": 1,
+    "genre_ids": [2, 3, 4],
+    "overview": "sinopse do filme",
+    "poster_path":"poster/path",
+    "release_date":"1977",
+    "title":"Xenogears the Movie",
+    "vote_average": 95
+  }
+  ''';

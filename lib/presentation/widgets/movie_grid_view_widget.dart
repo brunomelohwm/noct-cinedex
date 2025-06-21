f877c291d66577329907eab09e5a05767c1bf2ba
Bruno Melo <brunomelohwm@gmail.com>
2025-06-21 00:26:23 -0300

    feat: adjusts for moview grid view afters responsive components

--- lib/presentation/widgets/movie_grid_view_widget.dart
+++ lib/presentation/widgets/movie_grid_view_widget.dart
@@ -1,17 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:noct_cinedex/core/responsive/responsive_config.dart';
+import 'package:noct_cinedex/core/responsive/screen_type.dart';
 import 'package:noct_cinedex/domain/entities/movies/movie_entity.dart';
 import 'package:noct_cinedex/presentation/widgets/movie_list_item.widget.dart';
 
 class MovieGridView extends StatelessWidget {
   final List<MovieEntity> movies;
-  const MovieGridView({required this.movies, super.key});
+  final ScreenType screenType;
+  const MovieGridView({
+    super.key,
+    required this.movies,
+    required this.screenType,
+  });
 
   @override
   Widget build(BuildContext context) {
+    final config = ResponsiveConfig.of(screenType);
     return GridView.builder(
+      padding: const EdgeInsets.all(16),
       gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-        crossAxisCount: 2,
-        mainAxisExtent: 300,
+        crossAxisCount: config.crossAxisCount,
+        mainAxisExtent: config.mainAxisExtent,
+        mainAxisSpacing: 16,
+        crossAxisSpacing: 16,
       ),
       itemCount: movies.length,
       itemBuilder: (_, int index) {

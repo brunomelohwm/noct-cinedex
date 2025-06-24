059716b5169cdc310dde1bb88163db257e33de6b
Bruno Melo <brunomelohwm@gmail.com>
2025-06-23 22:09:12 -0300

    feat: added aspect ratio and removed fixed card sizes

--- lib/presentation/widgets/movie_back_card_widget.dart
+++ lib/presentation/widgets/movie_back_card_widget.dart
@@ -1,74 +1,76 @@
 import 'package:flutter/material.dart';
+import 'package:noct_cinedex/core/responsive/responsive_config.dart';
+import 'package:noct_cinedex/core/responsive/responsive_text_style.dart';
+import 'package:noct_cinedex/core/responsive/screen_type.dart';
 import 'package:noct_cinedex/domain/entities/movies/movie_entity.dart';
 
 class MovieBackCard extends StatelessWidget {
   final MovieEntity movie;
   final String imageUrl;
+  final ScreenType screenType;
 
-  const MovieBackCard({super.key, required this.movie, required this.imageUrl});
+  const MovieBackCard({
+    super.key,
+    required this.movie,
+    required this.imageUrl,
+    required this.screenType,
+  });
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      key: const ValueKey('back'),
-      margin: const EdgeInsets.symmetric(horizontal: 12, vertical: 8),
-      elevation: 3,
-      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
-      child: ClipRRect(
-        borderRadius: BorderRadius.circular(12),
-        child: Stack(
-          children: [
-            Image.network(imageUrl, height: 300, width: 200, fit: BoxFit.fill),
-            Container(
-              height: 300,
-              width: 200,
-              decoration: BoxDecoration(
-                gradient: LinearGradient(
-                  colors: [
-                    Colors.black.withValues(alpha: 0.8),
-                    Colors.black.withValues(alpha: 0.2),
-                  ],
-                  begin: Alignment.bottomCenter,
-                  end: Alignment.topCenter,
+    return AspectRatio(
+      aspectRatio: 3 / 5,
+      child: Card(
+        key: const ValueKey('back'),
+        margin: const EdgeInsets.symmetric(horizontal: 12, vertical: 8),
+        elevation: 3,
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
+        child: ClipRRect(
+          borderRadius: BorderRadius.circular(12),
+          child: Stack(
+            children: [
+              Positioned.fill(child: Image.network(imageUrl, fit: BoxFit.fill)),
+              Container(
+                decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                    colors: [
+                      Colors.black.withValues(alpha: 0.8),
+                      Colors.black.withValues(alpha: 0.2),
+                    ],
+                    begin: Alignment.bottomCenter,
+                    end: Alignment.topCenter,
+                  ),
                 ),
               ),
-            ),
-            Positioned.fill(
-              child: Padding(
-                padding: const EdgeInsets.all(12.0),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      movie.title,
-                      style: Theme.of(context).textTheme.titleMedium?.copyWith(
-                        color: Colors.white,
-                        fontWeight: FontWeight.bold,
+              Positioned.fill(
+                child: Padding(
+                  padding: const EdgeInsets.all(12.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        movie.title,
+                        style: ResponsiveTextStyle.cardSinopse(screenType),
+                        maxLines: 2,
+                        overflow: TextOverflow.ellipsis,
                       ),
-                      maxLines: 2,
-                      overflow: TextOverflow.ellipsis,
-                    ),
-                    const SizedBox(height: 8),
-                    Expanded(
-                      child: SingleChildScrollView(
-                        child: Text(
-                          movie.overview.isNotEmpty
-                              ? movie.overview
-                              : "Sinopse não disponível",
-                          style: Theme.of(
-                            context,
-                          ).textTheme.bodySmall?.copyWith(
-                            color: Colors.white70,
-                            fontWeight: FontWeight.bold,
+                      const SizedBox(height: 8),
+                      Expanded(
+                        child: SingleChildScrollView(
+                          child: Text(
+                            movie.overview.isNotEmpty
+                                ? movie.overview
+                                : "Sinopse não disponível",
+                            style: ResponsiveTextStyle.cardSinopse(screenType),
                           ),
                         ),
                       ),
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );

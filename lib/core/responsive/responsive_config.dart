a6828b836ab6997ea47aab0e7bb2fdfe8421bb5f
Bruno Melo <brunomelohwm@gmail.com>
2025-06-23 22:04:52 -0300

    feat: add new configs

--- lib/core/responsive/responsive_config.dart
+++ lib/core/responsive/responsive_config.dart
@@ -3,20 +3,59 @@ import 'package:noct_cinedex/core/responsive/screen_type.dart';
 class ResponsiveConfig {
   final int crossAxisCount;
   final double mainAxisExtent;
+  final double maxCrossAxisExtent;
+  final double cardHeight;
+  final double cardWidth;
+  final double spacing;
+  final double borderRadius;
+  final double gridPadding;
 
   const ResponsiveConfig({
     required this.crossAxisCount,
     required this.mainAxisExtent,
+    required this.maxCrossAxisExtent,
+    required this.cardHeight,
+    required this.cardWidth,
+    required this.spacing,
+    required this.borderRadius,
+    required this.gridPadding,
   });
 
   static ResponsiveConfig of(ScreenType type) {
     switch (type) {
       case ScreenType.mobile:
-        return const ResponsiveConfig(crossAxisCount: 2, mainAxisExtent: 300);
+        return const ResponsiveConfig(
+          crossAxisCount: 2,
+          mainAxisExtent: 300,
+          maxCrossAxisExtent: 180,
+          cardWidth: 150,
+          cardHeight: 250,
+          spacing: 6,
+          borderRadius: 8,
+          gridPadding: 8,
+        );
       case ScreenType.tablet:
-        return const ResponsiveConfig(crossAxisCount: 3, mainAxisExtent: 350);
+        return const ResponsiveConfig(
+          crossAxisCount: 2,
+          mainAxisExtent: 350,
+          maxCrossAxisExtent: 250,
+          cardWidth: 180,
+          cardHeight: 300,
+          spacing: 12,
+          borderRadius: 12,
+          gridPadding: 24,
+        );
       case ScreenType.totem:
-        return const ResponsiveConfig(crossAxisCount: 4, mainAxisExtent: 400);
+        return const ResponsiveConfig(
+          crossAxisCount: 2,
+          mainAxisExtent: 500,
+          maxCrossAxisExtent: 360,
+          cardWidth: 300,
+          cardHeight: 500,
+          spacing: 80,
+          borderRadius: 24,
+          gridPadding: 60,
+        );
     }
   }
 }
